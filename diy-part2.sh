6fef4ef0cb33b9f68f41d85f46d148df76f58575
yeyinghun <62594952+yeyinghun@users.noreply.github.com>
2021-03-23 22:30:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,4 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+cd lede/package/lean/ && git clone https://github.com/jerrykuku/lua-maxminddb.git && git clone https://github.com/jerrykuku/luci-app-vssr.git
